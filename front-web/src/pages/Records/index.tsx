95e2e9230cc0702c1f62deb067a21509a096f36c
Pato <pato@pop-os.localdomain>
2020-09-17 17:08:27 -0300

    Criando pagina de listagem dos Registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -2,22 +2,34 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles.css';
 import { RecordsResponse } from './types';
+import {formatDate} from './helpers';
+import Pagination from './Pagination'
+import { Link} from 'react-router-dom';
 
 const BASE_URL = 'http://localhost:8080';
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
-    console.log(recordsResponse);
+    const [activePage, setActivePage] = useState(0);
     useEffect(() => {
-        axios.get(`${BASE_URL}/records?linesPerPage=12`) // contatenacao de string
-        .then((response) =>setRecordsResponse(response.data))}, []);
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`) // contatenacao de string
+        .then((response) =>setRecordsResponse(response.data))}, [activePage]);
     
-
+    const handlePageChange = (index: number) =>{
+        setActivePage(index);
+    }
   
 
 
     return  (
         <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">
+                        VER GRÁFICOS
+                    </button>
+                </Link>
+            </div>
            <table className="records-table" cellPadding="0" cellSpacing="0">
                <thead>
                    <tr>
@@ -32,17 +44,23 @@ const Records = () => {
                </thead>
                <tbody>
                {recordsResponse?.content.map((record )=>(
-                      <tr key={record.id}>
-                      <td>20/08/2010 15:35</td>
-                      <td>{record.name}</td>
-                      <td>25</td>
-                      <td>XBOX</td>
-                      <td>AÇÃO</td>
-                      <td>The Last of US</td>
+                    <tr key={record.id}>
+                    <td>{formatDate(record.moment)}</td>
+                    <td>{record.name}</td>
+                    <td>{record.age}</td>
+                    <td className="text-secondary">{record.gamePlatform}</td>
+                    <td>{record.genreName}</td>
+                    <td className="text-primary">{record.gameTitle}</td>
                   </tr>
                    ))}
                </tbody>
-            </table> 
+            </table>
+            <Pagination 
+                activePage={activePage}
+                goToPage={handlePageChange }
+                totalPages={recordsResponse?.totalPages}
+            />
+
         </div>
     );
     
