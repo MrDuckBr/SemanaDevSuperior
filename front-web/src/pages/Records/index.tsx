82c86e4d60276bc39422ee110884c00a4894fb62
MrDuckBr <wwalisson2012@gmail.com>
2020-09-16 21:04:51 -0300

    :bug: Arrumar bug 1:40h

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css';
+import { RecordsResponse } from './types';
 
-const Records = () => (
-    <h1>Records</h1>
-);
+const BASE_URL = 'http://localhost:8080';
+
+const Records = () => {
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    console.log(recordsResponse);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12`) // contatenacao de string
+        .then((response) =>setRecordsResponse(response.data))}, []);
+    
+
+  
+
+
+    return  (
+        <div className="page-container">
+           <table className="records-table" cellPadding="0" cellSpacing="0">
+               <thead>
+                   <tr>
+                       <th>INSTANTE</th>
+                       <th>NOME</th>
+                       <th>IDADE</th>
+                       <th>PLATAFORMA</th>
+                       <th>GÊNERO</th>
+                       <th>TITULO DO GAME</th>
+    
+                   </tr>
+               </thead>
+               <tbody>
+               {recordsResponse?.content.map((record )=>(
+                      <tr key={record.id}>
+                      <td>20/08/2010 15:35</td>
+                      <td>{record.name}</td>
+                      <td>25</td>
+                      <td>XBOX</td>
+                      <td>AÇÃO</td>
+                      <td>The Last of US</td>
+                  </tr>
+                   ))}
+               </tbody>
+            </table> 
+        </div>
+    );
+    
+}
 
 export default Records;
